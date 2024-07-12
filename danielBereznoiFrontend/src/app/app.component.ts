4e658cf48abb0c10231a03d9e96f9e881a74f422
Daniel Bereznoi <danielbereznoi@gmail.com>
2024-07-12 21:03:58 +0300

    Home screen done Started workign on booking page Fixes Create a home screen #6

--- danielBereznoiFrontend/src/app/app.component.ts
+++ danielBereznoiFrontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { BookingComponent } from './booking/booking.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, BookingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
